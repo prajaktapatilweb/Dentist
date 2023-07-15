20a6182521a2f6013b5413c733d28b6ed0936718
Prajakta_Laptop <prajaktaweb@gmail.com>
2023-07-15 16:15:07 +0530

    Send Email Succesful 3

--- src/components/home/Faq.js
+++ src/components/home/Faq.js
@@ -7,12 +7,8 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Container, Box } from '@mui/material';
 
 export default function Faq() {
-    var Details = [
-        {
-            title: "",
-            para: ""
-        }
-    ]
+  
+    
     return (
         <div>
             <Box sx={{
@@ -30,20 +26,20 @@ export default function Faq() {
                             md: 5,
                         },
                     }} gutterBottom>
-                        FAQ :
+                        FAQs :
                     </Typography>
+
                     <Accordion>
                         <AccordionSummary
                             expandIcon={<ExpandMoreIcon />}
                             aria-controls="panel1a-content"
                             id="panel1a-header"
                         >
-                            <Typography>Accordion 1</Typography>
+                            <Typography> How often should I visit the dentist for a regular check-up and cleaning?</Typography>
                         </AccordionSummary>
                         <AccordionDetails>
                             <Typography>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                                malesuada lacus ex, sit amet blandit leo lobortis eget.
+                                It is generally recommended to visit the dentist for a regular check-up and cleaning every 4 months. However, the frequency may vary depending on your oral health needs. Your dentist will provide a personalized recommendation based on your specific dental condition.
                             </Typography>
                         </AccordionDetails>
                     </Accordion>
@@ -53,15 +49,43 @@ export default function Faq() {
                             aria-controls="panel2a-content"
                             id="panel2a-header"
                         >
-                            <Typography>Accordion 2</Typography>
+                            <Typography> Can you provide options for replacing missing teeth, such as dental implants or dentures?</Typography>
                         </AccordionSummary>
                         <AccordionDetails>
                             <Typography>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                                malesuada lacus ex, sit amet blandit leo lobortis eget.
+                                Absolutely. We specialize in dental implants and offer various options for replacing missing teeth, including dental implants, dentures, and dental bridges.
                             </Typography>
                         </AccordionDetails>
                     </Accordion>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel2a-content"
+                            id="panel2a-header"
+                        >
+                            <Typography>  Do you offer cosmetic dentistry services such as teeth whitening or veneers?</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                Yes, we offer cosmetic dentistry services such as teeth whitening and veneers.
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel2a-content"
+                            id="panel2a-header"
+                        >
+                            <Typography>What payment options do you accept, and do you offer any dental financing or insurance options?</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                We accept various payment options, including cash, credit cards, debit cards, and UPI.
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+
                 </Container>
             </Box>
         </div>
